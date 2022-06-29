56c29ee54fc6bfbf9cbabcc968ac2c9c89128f47
sh <aotushi@hotmail.com>
2022-06-29 17:42:06 +0800

    vue/todo-list

---
+++ note4/todo-list/src/components/TodoList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input
+      class="new-todo"
+      placeholder="你接下来要做什么"
+      v-model="newTodo"
+      v-on:keyup.enter="addTodo"
+    />
+  </div>
+</template>
+
+<script>
+  let id=1;
+  export default {
+    name:'TodoList',
+    data() {
+      return {
+        todos: [],
+        newTodo: '',
+        editTodo: {}
+      }
+    },
+    methods: {
+      addTodo() {
+        // 如果没有输入，则什么都不做
+        if (!this.newTodo) return;
+
+        // 新添加的备忘录，添加在列表的前面
+        this.todos.unshift({
+          id: id++,
+          complete: false,
+          title: this.newTodo
+        });
+
+        // 备忘项添加到列表后，清空接收项
+        this.newTodo = '';
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  @import "https://unpkg.com/todomvc-app-css@2.1.0/index.css";
+</style>
